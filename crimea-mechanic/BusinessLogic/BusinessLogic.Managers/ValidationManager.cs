31cce36d1437f668aeb48989eddbdd13361589e0
Илья Мжачев <i.mzhachev@solarl.ru>
2017-11-02 00:54:11 +0300

    Тесты для менеджера валидации + реализация валидации дто регистрации пользователя

--- crimea-mechanic/BusinessLogic/BusinessLogic.Managers/ValidationManager.cs
+++ crimea-mechanic/BusinessLogic/BusinessLogic.Managers/ValidationManager.cs
@@ -1,5 +1,8 @@
-﻿using BusinessLogic.Managers.Abstraction;
+﻿using System;
+using System.Text.RegularExpressions;
+using BusinessLogic.Managers.Abstraction;
 using BusinessLogic.Objects.User;
+using BusinessLogic.Resources;
 using Common.Validation;
 using DataAccessLayer.Repositories.Abstraction;
 
@@ -20,14 +23,59 @@ namespace BusinessLogic.Managers
 
         public ValidationResult ValidateRegistrationDto(RegistrationDto dto)
         {
-            throw new System.NotImplementedException();
+            if (dto == null)
+            {
+                throw new ArgumentNullException(ArgumentExceptionResources.RegistrationDtoNotFound);
+            }
+            var validatioResult = new ValidationResult("Регистрация пользователя");
+            var isRegular = dto is RegistrationUserDto;
+
+            ValidateRegistartionBaseDto(dto, validatioResult);
+            if (isRegular)
+            {
+                ValidateRegistrationUserDto((RegistrationUserDto) dto, validatioResult);
+            }
+            else
+            {
+                ValidateRegistrationCarServiceDto((RegistrationCarServiceDto) dto, validatioResult);
+            }
+            return validatioResult;
         }
 
         #endregion
 
         #region Private methods
 
-        
+        private void ValidateRegistartionBaseDto(RegistrationDto dto, ValidationResult validationResult)
+        {
+            var loginRegex = new Regex("^\\w[\\w-\\.]*@\\w+\\.\\w{2,4}$", RegexOptions.IgnoreCase);
+            if (!string.IsNullOrEmpty(dto.Login) && !loginRegex.IsMatch(dto.Login))
+            {
+                validationResult.AddError(ValidationErrorResources.LoginIsNotValid);
+            }
+            if (!dto.Password.Equals(dto.PasswordСonfirmation))
+            {
+                validationResult.AddError(ValidationErrorResources.PasswordsNotMatch);
+            }
+        }
+
+        private void ValidateRegistrationUserDto(RegistrationUserDto dto, ValidationResult validationResult)
+        {
+            if (string.IsNullOrEmpty(dto.ContactName))
+            {
+                validationResult.AddError(ValidationErrorResources.ContactNameIsEmpty);
+            }
+            var phoneRegex = new Regex("^\\+7(\\d{3}|\\(\\d{3}\\))\\-?\\d{3}\\-?\\d{2}\\-?\\d{2}$");
+            if (!string.IsNullOrEmpty(dto.Phone) && !phoneRegex.IsMatch(dto.Phone))
+            {
+                validationResult.AddError(ValidationErrorResources.PhoneNumberIsIncorrect);
+            }
+        }
+
+        private void ValidateRegistrationCarServiceDto(RegistrationCarServiceDto dto, ValidationResult validationResult)
+        {
+
+        }
 
         #endregion
     }
--- crimea-mechanic/UnitTests/BusinessLogic.UnitTests/ValidationManagerTests.cs
+++ crimea-mechanic/UnitTests/BusinessLogic.UnitTests/ValidationManagerTests.cs
@@ -1,6 +1,8 @@
-﻿using BusinessLogic.Managers;
+﻿using System;
+using BusinessLogic.Managers;
 using BusinessLogic.Managers.Abstraction;
 using BusinessLogic.Objects.User;
+using BusinessLogic.Resources;
 using Common.Enums;
 using Common.Validation;
 using DataAccessLayer.Repositories.Abstraction;
@@ -41,8 +43,17 @@ namespace BusinessLogic.UnitTests
 
         #region ValidateRegistrationDto
 
+        [Test(Description = "ValidateRegistrationDto должен кинуть ошибку если не передана дто")]
+        public void ValidateRegistrationDto_Must_Throw_Exception_When_Dto_Not_Found()
+        {
+            //Arrange
+            //Act & Assert
+            var exception = Assert.Throws<ArgumentNullException>(() => _manager.ValidateRegistrationDto(null));
+            Assert.AreEqual(ArgumentExceptionResources.RegistrationDtoNotFound, exception.ParamName);
+        }
+
         [Test(Description = "ValidateRegistrationDto должен провалидировать дто регистрации обычного пользователя без ошибок")]
-        public void ValidateRegistrationto_Must_Validate_Registration_User_Dto_Without_Errors()
+        public void ValidateRegistrationDto_Must_Validate_Registration_User_Dto_Without_Errors()
         {
             //Arrange
             var dto = new RegistrationUserDto
@@ -61,6 +72,36 @@ namespace BusinessLogic.UnitTests
 
             //Assert
             Assert.False(result.HasErrors);
+        }
+
+        [Test(Description = "ValidateRegistrationDto должен провалидировать дто регистрации обычного пользователя и вернуть все возможные ошибки")]
+        public void ValidateRegistrationDto_Must_Validate_Registration_User_Dto_With_All_Possible_Errors()
+        {
+            //Arrange
+            var dto = new RegistrationUserDto
+            {
+                ContactName = "",
+                Login = "test@@test.com",
+                Password = "qwerty",
+                PasswordСonfirmation = "qwerty1",
+                Phone = "+7(111)-111-11-11312",
+                Type = RegistrationTypeEnum.Regular
+            };
+
+            var exceptions = string.Join(";",
+                ValidationErrorResources.LoginIsNotValid,
+                ValidationErrorResources.PasswordsNotMatch,
+                ValidationErrorResources.ContactNameIsEmpty,
+                ValidationErrorResources.PhoneNumberIsIncorrect);
+            var exceptionMessage = $"Регистрация пользователя: {exceptions}";
+
+            //Act
+            ValidationResult result = null;
+            Assert.DoesNotThrow(() => result = _manager.ValidateRegistrationDto(dto));
+
+            //Assert
+            Assert.True(result.HasErrors);
+            Assert.AreEqual(exceptionMessage, result.GetErrors());
         }
 
         #endregion
