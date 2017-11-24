0a34abd5d8ea2131576bd1023cde388418ce5774
Илья Мжачев <i.mzhachev@solarl.ru>
2017-11-24 22:41:04 +0300

    Мультиязычность + Хлебные крошки + Улучшения по UI

--- crimea-mechanic/WebUI/App_Start/RouteConfig.cs
+++ crimea-mechanic/WebUI/App_Start/RouteConfig.cs
@@ -8,11 +8,17 @@ namespace WebUI
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute(
+                name: "lang",
+                url: "{lang}/{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                constraints: new { lang = @"ru|en" }
+            );
 
             routes.MapRoute(
-                name: "Default",
+                name: "default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional, lang = "ru" }
             );
         }
     }
---
+++ crimea-mechanic/WebUI/Helpers/LangHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebUI.Helpers
+{
+    public static class LangHelper
+    {
+        public static MvcHtmlString LangSwitch(this UrlHelper url, RouteData routeData, string lang)
+        {
+            var routeValueDictionary = new RouteValueDictionary(routeData.Values);
+            if (routeValueDictionary.ContainsKey("lang"))
+            {
+                routeValueDictionary["lang"] = lang;
+            }
+            return new MvcHtmlString(url.RouteUrl(routeValueDictionary));
+        }
+    }
+}
