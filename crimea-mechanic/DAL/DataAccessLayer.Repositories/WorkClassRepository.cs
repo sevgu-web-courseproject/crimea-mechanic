e559475667d3679427a9a8325797da9eb6d67aec
Илья Мжачев <i.mzhachev@solarl.ru>
2017-12-02 22:22:50 +0300

    Добавил репозитории

---
+++ crimea-mechanic/DAL/DataAccessLayer.Repositories/WorkClassRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+using DataAccessLayer.Models;
+using DataAccessLayer.Repositories.Abstraction;
+
+namespace DataAccessLayer.Repositories
+{
+    public class WorkClassRepository : RepositoryBase<WorkClass, long>, IWorkClassRepository
+    {
+        public WorkClassRepository(DbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
