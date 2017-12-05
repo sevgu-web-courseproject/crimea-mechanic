0b2b5382afba11670c64a296343272e15a503eb8
Илья Мжачев <i.mzhachev@solarl.ru>
2017-12-05 22:46:32 +0300

    Реализация интерефейса для работы со справочником

--- crimea-mechanic/BusinessLogic/BusinessLogic.Managers/MapProfiles/CarModelProfile.cs
+++ crimea-mechanic/BusinessLogic/BusinessLogic.Managers/MapProfiles/CarModelProfile.cs
@@ -1,5 +1,7 @@
-﻿using AutoMapper;
+﻿using System;
+using AutoMapper;
 using BusinessLogic.Objects;
+using BusinessLogic.Objects.Storage;
 using DataAccessLayer.Models;
 
 namespace BusinessLogic.Managers.MapProfiles
@@ -9,6 +11,14 @@ namespace BusinessLogic.Managers.MapProfiles
         public CarModelProfile()
         {
             CreateMap<CarModel, CarModelDto>();
+
+            CreateMap<AddCarModelDto, CarModel>()
+                .ForMember(d => d.Id, opt => opt.Ignore())
+                .ForMember(d => d.IsDeleted, opt => opt.UseValue(false))
+                .ForMember(d => d.Created, opt => opt.MapFrom(s => DateTime.UtcNow))
+                .ForMember(d => d.Updated, opt => opt.MapFrom(s => DateTime.UtcNow))
+                .ForMember(d => d.Mark, opt => opt.Ignore())
+                .ForMember(d => d.UserCars, opt => opt.Ignore());
         }
     }
 }
--- crimea-mechanic/BusinessLogic/BusinessLogic.Managers/StorageManager.cs
+++ crimea-mechanic/BusinessLogic/BusinessLogic.Managers/StorageManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using AutoMapper;
@@ -8,6 +9,7 @@ using BusinessLogic.Objects.Storage;
 using BusinessLogic.Objects.Works;
 using BusinessLogic.Resources;
 using Common.Exceptions;
+using DataAccessLayer.Models;
 using DataAccessLayer.Repositories.Abstraction;
 
 namespace BusinessLogic.Managers
@@ -79,52 +81,159 @@ namespace BusinessLogic.Managers
 
         public void AddCity(AddCityDto dto, string currentUserId)
         {
-            throw new System.NotImplementedException();
+            UserManager.IsUserInAdministrationRole(currentUserId);
+            var repository = UnitOfWork.Repository<ICityRepository>();
+            var city = repository.GetAll(true).SingleOrDefault(c => !c.IsDeleted && c.Name == dto.Name);
+            if (city != null)
+            {
+                throw new BusinessFaultException(BusinessLogicExceptionResources.CityAlreadyContains);
+            }
+            city = Mapper.Map<City>(dto);
+            repository.Add(city);
+            UnitOfWork.SaveChanges();
         }
 
         public void AddCarMark(AddCarMarkDto dto, string currentUserId)
         {
-            throw new System.NotImplementedException();
+            UserManager.IsUserInAdministrationRole(currentUserId);
+            var repository = UnitOfWork.Repository<ICarMarksRepository>();
+            var mark = repository.GetAll(true).SingleOrDefault(m => !m.IsDeleted && m.Name == dto.Name);
+            if (mark != null)
+            {
+                throw new BusinessFaultException(BusinessLogicExceptionResources.CarMarkAlreadyContains);
+            }
+            mark = Mapper.Map<CarMark>(dto);
+            repository.Add(mark);
+            UnitOfWork.SaveChanges();
         }
 
         public void AddCarModel(AddCarModelDto dto, string currentUserId)
         {
-            throw new System.NotImplementedException();
+            UserManager.IsUserInAdministrationRole(currentUserId);
+            var repository = UnitOfWork.Repository<ICarMarksRepository>();
+            var mark = repository.Get(dto.MardId);
+            if (mark == null)
+            {
+                throw new BusinessFaultException(BusinessLogicExceptionResources.CarMarkNotFound);
+            }
+            var model = mark.Models.SingleOrDefault(m => !m.IsDeleted && m.Name == dto.Name);
+            if (model != null)
+            {
+                throw new BusinessFaultException(BusinessLogicExceptionResources.CarModelAlreadyContains);
+            }
+            model = Mapper.Map<CarModel>(dto);
+            mark.Models.Add(model);
+            repository.Update(mark);
+            UnitOfWork.SaveChanges();
         }
 
         public void AddWorkClass(AddWorkClassDto dto, string currentUserId)
         {
-            throw new System.NotImplementedException();
+            UserManager.IsUserInAdministrationRole(currentUserId);
+            var repository = UnitOfWork.Repository<IWorkClassRepository>();
+            var workClass = repository.GetAll(true).SingleOrDefault(w => !w.IsDeleted && w.Name == dto.Name);
+            if (workClass != null)
+            {
+                throw new BusinessFaultException(BusinessLogicExceptionResources.WorkClassAlreadyContains);
+            }
+            workClass = Mapper.Map<WorkClass>(dto);
+            repository.Add(workClass);
+            UnitOfWork.SaveChanges();
         }
 
         public void AddWorkType(AddWorkTypeDto dto, string currentUserId)
         {
-            throw new System.NotImplementedException();
+            UserManager.IsUserInAdministrationRole(currentUserId);
+            var repository = UnitOfWork.Repository<IWorkClassRepository>();
+            var workClass = repository.Get(dto.WorkClassId);
+            if (workClass == null)
+            {
+                throw new BusinessFaultException(BusinessLogicExceptionResources.WorkClassNotFound);
+            }
+            var workType = workClass.WorkTypes.SingleOrDefault(w => !w.IsDeleted && w.Name == dto.Name);
+            if (workType != null)
+            {
+                throw new BusinessFaultException(BusinessLogicExceptionResources.WorkTypeAlreadyContains);
+            }
+            workType = Mapper.Map<WorkType>(dto);
+            workClass.WorkTypes.Add(workType);
+            repository.Update(workClass);
+            UnitOfWork.SaveChanges();
         }
 
         public void DeleteCity(long cityId, string currentUserId)
         {
-            throw new System.NotImplementedException();
+            UserManager.IsUserInAdministrationRole(currentUserId);
+            var repository = UnitOfWork.Repository<ICityRepository>();
+            var city = repository.Get(cityId);
+            if (city == null)
+            {
+                throw new BusinessFaultException(BusinessLogicExceptionResources.CityNotFound);
+            }
+            city.IsDeleted = true;
+            city.Updated = DateTime.UtcNow;
+            repository.Update(city);
+            UnitOfWork.SaveChanges();
         }
 
         public void DeleteMark(long markId, string currentUserId)
         {
-            throw new System.NotImplementedException();
+            UserManager.IsUserInAdministrationRole(currentUserId);
+            var repository = UnitOfWork.Repository<ICarMarksRepository>();
+            var mark = repository.Get(markId);
+            if (mark == null)
+            {
+                throw new BusinessFaultException(BusinessLogicExceptionResources.CarMarkNotFound);
+            }
+            mark.IsDeleted = true;
+            mark.Updated = DateTime.UtcNow;
+            repository.Update(mark);
+            UnitOfWork.SaveChanges();
         }
 
         public void DeleteModel(long modelId, string currentUserId)
         {
-            throw new System.NotImplementedException();
+            UserManager.IsUserInAdministrationRole(currentUserId);
+            var repository = UnitOfWork.Repository<ICarModelsRepository>();
+            var model = repository.Get(modelId);
+            if (model == null)
+            {
+                throw new BusinessFaultException(BusinessLogicExceptionResources.CarModelNotFound);
+            }
+            model.IsDeleted = true;
+            model.Updated = DateTime.UtcNow;
+            repository.Update(model);
+            UnitOfWork.SaveChanges();
         }
 
         public void DeleteWorkClass(long workClassId, string currentUserId)
         {
-            throw new System.NotImplementedException();
+            UserManager.IsUserInAdministrationRole(currentUserId);
+            var repository = UnitOfWork.Repository<IWorkClassRepository>();
+            var workClass = repository.Get(workClassId);
+            if (workClass == null)
+            {
+                throw new BusinessFaultException(BusinessLogicExceptionResources.WorkClassNotFound);
+            }
+            workClass.IsDeleted = true;
+            workClass.Updated = DateTime.UtcNow;
+            repository.Update(workClass);
+            UnitOfWork.SaveChanges();
         }
 
         public void DeleteWorkType(long workTypeId, string currentUserId)
         {
-            throw new System.NotImplementedException();
+            UserManager.IsUserInAdministrationRole(currentUserId);
+            var repository = UnitOfWork.Repository<IWorkTypeRepository>();
+            var workType = repository.Get(workTypeId);
+            if (workType == null)
+            {
+                throw new BusinessFaultException(BusinessLogicExceptionResources.WorkTypeNotFound);
+            }
+            workType.IsDeleted = true;
+            workType.Updated = DateTime.UtcNow;
+            repository.Update(workType);
+            UnitOfWork.SaveChanges();
         }
 
         #endregion
